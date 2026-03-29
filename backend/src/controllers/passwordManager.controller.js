a3309cf7b42ef3e830076aaddbbd11ddbabe6065
Nikhil <nikhilkanojia693@gmail.com>
2026-03-30 01:45:52 +0530

    feat: add list and get password by ID functionality to password manager

--- backend/src/controllers/passwordManager.controller.js
+++ backend/src/controllers/passwordManager.controller.js
@@ -16,3 +16,45 @@ export const savePassword = async (req, res) => {
         message: "Password saved/secured successfully!",
     });
 };
+
+export const listPasswords = async (req, res) => {
+    const { id } = req.user;
+    const passwords = await PasswordManager.find({
+        userId: id,
+    });
+
+    if (passwords.length === 0) {
+        return res.status(400).json({
+            message: "Didn't Find Any Saved Passwords",
+        });
+    }
+
+    res.status(200).json({
+        message: "Passwords Fetched Successfully!",
+        passwords: passwords,
+    });
+};
+
+export const getPasswordById = async (req, res) => {
+    const { id } = req.user;
+    const { id: passId } = req.params;
+
+    const passwordById = await PasswordManager.findOne({
+        _id: passId,
+        userId: id,
+    });
+
+    if (!passwordById) {
+        return res.status(404).json({
+            message: "Password Does Not Exist",
+        });
+    }
+
+    res.status(200).json({
+        message: "Password Fetched Successfully",
+        data: {
+            username: passwordById.username,
+            password: passwordById.decryptPassword(),
+        },
+    });
+};
--- backend/src/routes/passManager.route.js
+++ backend/src/routes/passManager.route.js
@@ -1,9 +1,17 @@
 import express from "express";
 import requireAuth from "../middlewares/auth.middleware.js";
-import { savePassword } from "../controllers/passwordManager.controller.js";
+import {
+    getPasswordById,
+    listPasswords,
+    savePassword,
+} from "../controllers/passwordManager.controller.js";
 
 const router = express.Router();
 
 router.post("/save-password", requireAuth, savePassword);
+router.get("/list-passwords", requireAuth, listPasswords);
+router.get("/get-password/:id", requireAuth, getPasswordById);
+// router.put("/update-password/:id", requireAuth, updatePassword);
+// router.delete("/delete-password/:id", requireAuth, deletePassword);
 
 export default router;
